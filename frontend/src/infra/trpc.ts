e3e6396d0942eb4879626feb0ff21e1c50bc2d44
Kay Gosho <ketsume0211@gmail.com>
2024-09-01 16:34:07 +0900

    refactor: trpc header

--- frontend/src/infra/trpc.ts
+++ frontend/src/infra/trpc.ts
@@ -23,7 +23,7 @@ export const useTrpcClient = () => {
           // You can pass any HTTP headers you wish here
           async headers() {
             return {
-              authorization: 'Bearer hogehoge',
+              'x-client': 'trpc',
             }
           },
         }),
@@ -34,13 +34,8 @@ export const useTrpcClient = () => {
 }
 
 export const errorToastLink: TRPCLink<AppRouter> = () => {
-  // here we just got initialized in the app - this happens once per app
-  // useful for storing cache for instance
   return ({ next, op }) => {
-    // this is when passing the result to the next link
-    // each link needs to return an observable which propagates results
     return observable((observer) => {
-      console.log('performing operation:', op)
       const unsubscribe = next(op).subscribe({
         next(value) {
           observer.next(value)
